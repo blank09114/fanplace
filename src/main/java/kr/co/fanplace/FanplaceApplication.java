8b1e31664845b020e97f2ba699492a0e3d76b930
blank0914 <yerin0373@gmail.com>
2026-01-13 22:38:30 +0900

    CI

--- src/main/java/kr/co/fanplace/FanplaceApplication.java
+++ src/main/java/kr/co/fanplace/FanplaceApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FanplaceApplication {
-
-	public static void main(String[] args) {
+public class FanplaceApplication
+{ public static void main(String[] args) {
 		SpringApplication.run(FanplaceApplication.class, args);
-	}
-
-}
+	} }
