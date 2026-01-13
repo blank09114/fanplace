d37a277aade6d4f961962fa3bd37da2aab8841aa
blank0914 <yerin0373@gmail.com>
2026-01-13 22:20:27 +0900

    Initial commit

---
+++ src/main/java/kr/co/fanplace/FanplaceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fanplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/fanplace/FanplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.fanplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
