47beb0db4b504dc75549bae30c15bb44ba526fd8
blank0914 <yerin0373@gmail.com>
2026-01-27 15:04:12 +0900

    페이지 세팅

--- src/main/java/kr/co/fanplace/controller/MainController.java
+++ src/main/java/kr/co/fanplace/controller/MainController.java
@@ -1,4 +1,17 @@
 package kr.co.fanplace.controller;
 
-public class MainController {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController
+{
+    // 메인
+    @GetMapping("/")
+    public String main()
+    {
+        return "main";
+    }
 }
